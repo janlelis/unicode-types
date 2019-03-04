bf3dcd5bc29fb632aad43b8262861ce5aa18f383
Jan Lelis <mail@janlelis.de>
2019-03-04 20:48:52 +0100

    Unicode 12

--- lib/unicode/types/constants.rb
+++ lib/unicode/types/constants.rb
@@ -1,7 +1,7 @@
 module Unicode
   module Types
     VERSION = "1.3.0".freeze
-    UNICODE_VERSION = "11.0.0".freeze
+    UNICODE_VERSION = "12.0.0".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/types.marshal.gz').freeze
   end
